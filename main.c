2efd44812921f6ef0e5d29d7222cb977147baab6
Sasindu50 <76037950+Sasindu50@users.noreply.github.com>
2020-12-29 18:51:51 +0530

    Update main.c

--- main.c
+++ main.c
@@ -3,24 +3,29 @@
 
 int main()
 {
-   int fib[10];
-   int i;
+   
+   int i,n;
+   
+   printf("Enter how long fibonacci series that you want : ");
+   scanf("%d",&n);
+   
+   int fib[n];
 
    fib[0] = 0;
    fib[1] = 1;
 
-   for(i=2; i<10; i++)
+   for(i=2; i<n; i++)
    {
        printf("Enter number %d : ",i-1);
        scanf("%d",&fib[1]);
    }
 
-   for(i=0; i<10; i++)
+   for(i=0; i<n; i++)
    {
        fib[i+2] = fib[i] + fib[i+1];
    }
 
-   for(i=0; i<5; i++)
+   for(i=0; i<n; i++)
    {
        printf("%d\t",fib[i]);
    }
